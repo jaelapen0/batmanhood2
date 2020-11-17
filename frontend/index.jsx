0eb16f3893e53042b902012b8329cf94934d8fc4
Jae Song <jae92kr@gmail.com>
2020-11-17 09:28:58 -0500

    user auth continued

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {login, logout, signup} from "./util/session_api_util"
 import configureStore from "./store/store"
-const store = configureStore();
+import Root from "./components/root";
+
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome Gang</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
     
     window.getState = store.getState;
-    
     window.dispatch = store.dispatch; // just for testing!
+
     // window.login = login;
     // window.logout = logout;
     // window.signup = signup;

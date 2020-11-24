2898dfb2c5ef197fc8d79db6836d4d3f4c7f5c68
Jae Song <jae92kr@gmail.com>
2020-11-24 11:54:38 -0500

    changed uri call

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -5,15 +5,16 @@ class Api::NewsController < ApplicationController
     #    https://newsapi.org/v2/everything?q=finance
         # url = "http://newsapi.org/v2/top-headlines?country=us&apiKey=88ee272f7d8e437ea3768f3847c82023"
         # url = "https://newsapi.org/v2/everything?q=wall%20street&apiKey=88ee272f7d8e437ea3768f3847c82023"
-         url = "https://gnews.io/api/v4/search?q=wall%20street&token=4d1d5f41f2ed41278fa8b717da618ab6&lang=en"
-        @response = JSON.parse(open(url).read)
+        
+        url = "https://gnews.io/api/v4/search?q=wall%20street&token=4d1d5f41f2ed41278fa8b717da618ab6&lang=en"
+        @response = JSON.parse(URI.open(url).read)
         render json: @response
     end
 
     def show
         ticker_symbol = params["ticker_symbol"]
         url = "https://newsapi.org/v2/everything?q=#{ticker_symbol}&apiKey=88ee272f7d8e437ea3768f3847c82023"
-        @response = JSON.parse(open(url).read)
+        @response = JSON.parse(URI.open(url).read)
         # debugger
         render json: @response
     end

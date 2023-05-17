043e33a30b5aa44b9472b06574f01754c2f5cc22
Jae Song <jae92kr@gmail.com>
2023-05-17 15:06:17 -0400

    updated logic to use stored news tokens

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -1,27 +1,18 @@
 require 'open-uri'
 
 class Api::NewsController < ApplicationController
-     def index
-    #    https://newsapi.org/v2/everything?q=finance
-        # url = "http://newsapi.org/v2/top-headlines?country=us&apiKey=88ee272f7d8e437ea3768f3847c82023"
-        # url = "https://newsapi.org/v2/everything?q=wall%20street&apiKey=88ee272f7d8e437ea3768f3847c82023"
-        
-        url = "https://gnews.io/api/v4/search?q=stocks%20market&token=4d1d5f41f2ed41278fa8b717da618ab6&lang=en"
+    def index
+        token = Token.where(name: "news_index").first.token
+        url = "https://gnews.io/api/v4/search?q=stocks%20market&token=#{token}&lang=en"
         @response = JSON.parse(URI.open(url).read)
         render json: @response
     end
 
     def show
         ticker_symbol = params["ticker_symbol"]
-        url = "https://newsapi.org/v2/everything?q=#{ticker_symbol}&apiKey=88ee272f7d8e437ea3768f3847c82023"
+        token = Token.where(name: "news_show").first.token
+        url = "https://newsapi.org/v2/everything?q=#{ticker_symbol}&apiKey=#{token}"
         @response = JSON.parse(URI.open(url).read)
-        # 
         render json: @response
     end
-
-    # private
-    # def news_params
-    #     params.require(:news).permit(:ticker_symbol)
-    # end
-
 end
